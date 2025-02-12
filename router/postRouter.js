559d8e2e2ef1ab83362e8b849a5dddf1eeec15d9
francescodelmonaco <francescodelmonaco1999@gmail.com>
2025-02-12 15:35:12 +0100

    add: postRouter.js

---
+++ router/postRouter.js
@@ -0,0 +1,19 @@
+const postController = require("./controllers/postController");
+
+// index
+router.get('/', postController.index);
+
+// show
+router.get('/:id', postController.show);
+
+// store
+router.post('/', postController.store);
+
+// update
+router.put('/:id', postController.update);
+
+// modify
+router.patch('/:id', postController.modify);
+
+// destroy
+router.delete('/:id', postController.destroy);
