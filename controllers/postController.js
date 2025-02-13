76981f4a386c017dbd61d5262a578be62e599d94
francescodelmonaco <francescodelmonaco1999@gmail.com>
2025-02-13 16:54:37 +0100

    add: update

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,17 +1,14 @@
 const posts = require('../data/posts');
 
 function index(req, res) {
-    // copiamo la logica dell'index
     res.json(posts);
 };
 
 function show(req, res) {
-    // copiamo la logica della show
     res.json(posts[req.params.id]);
 };
 
 function store(req, res) {
-    // copiamo la logica della store
     // Creiamo un nuovo id incrementando l'ultimo id presente
     const newId = posts[posts.length - 1].id + 1;
 
@@ -35,8 +32,35 @@ function store(req, res) {
 };
 
 function update(req, res) {
-    // copiamo la logica dell'update
-    res.send('Modifica integrale del post ' + req.params.id);
+    // recuperiamo l'id dall' URL e trasformiamolo in numero
+    const id = parseInt(req.params.id)
+
+    // cerchiamo il pizza tramite id
+    const post = posts.find(post => post.id === id);
+
+    // Piccolo controllo
+    if (!post) {
+        res.status(404);
+
+        return res.json({
+            status: 404,
+            error: "Not Found",
+            message: "Post non trovato"
+        })
+    };
+
+    // Aggiorniamo il post
+    post.title = req.body.title;
+    post.content = req.body.content;
+    post.image = req.body.image;
+    post.tags = req.body.tags;
+
+    // Controlliamo l'array
+    console.log(posts);
+
+    // Restituiamo il post appena aggiornato
+    res.json(post);
+
 };
 
 function modify(req, res) {
@@ -45,8 +69,6 @@ function modify(req, res) {
 }
 
 function destroy(req, res) {
-    // copiamo la logica della destroy
-
     // recuperiamo l'id dall' URL e trasformiamolo in numero
     const id = parseInt(req.params.id)
 
